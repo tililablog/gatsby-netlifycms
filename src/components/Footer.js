084525724048b9cf8c03ac7b7c0c7bc027c67c7c
tililablog <109964324+tililablog@users.noreply.github.com>
2022-07-25 19:53:35 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -94,7 +94,7 @@ const Footer = class extends React.Component {
               </div>
             </div>
           </div>
-          <p style={{paddingBottom:'1em', color:'#7c7c7c'}}>Copyright © yourdomain.com | 20XX</p>
+          <p style={{paddingBottom:'1em', color:'#7c7c7c'}}>Copyright © izzatilila.com | 2022</p>
         </div>
       </footer>
     )
